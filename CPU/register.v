12cb6fdae01dbddb6404f8254e4477f6d1995d20
jerdu13 <19jad7@queensu.ca>
2023-01-27 15:13:44 -0500

    register specification
    implementation for a 32 bit register

---
+++ CPU/register.v
@@ -0,0 +1,11 @@
+module register(input wire clr, clk, wrt_enable, input wire [31:0] D, output wire [31:0] Q); // D: input data, Q: output data, wrt_enable: R1in
+	
+	always @(posedge clk) begin
+		if(wrt_enable)
+			Q = D;
+	end
+	
+	always @(clr) begin
+		Q = 0;
+	end
+endmodule
