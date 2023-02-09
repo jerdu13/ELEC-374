064553944c9f82e4c36c2e56920c870126536b79
jerdu13 <19jad7@queensu.ca>
2023-02-08 21:05:47 -0500

    Update register_tb.v
    register testbench

--- CPU/register_tb.v
+++ CPU/register_tb.v
@@ -1,17 +1,42 @@
-module register_tb();
+module testbench();
 
-wire clk, clr, wrt_enable;
-reg[31:0] D, Q;
-
-register(clr, clk, wrt_enable, D, Q);
-
-initial
-	begin
-		#200
-			D = 32'b1111
-			wrt_enable = 1;	// Allowing D input from bus to be written to register
-			
-		#200
-			clr = 1;
+	reg clk;
+	reg [31:0] D;
+	reg wrt_enable;
+	reg clr;
+	wire [31:0] Q;
+	
+	register tb(clr, clk, wrt_enable, D, Q);
+	
+	initial begin
+		
+		clr = 0;
+		clk = 0;
+		D = 0;
+		wrt_enable = 0;
+		
+		#100;
+		
+		// write to register
+		D = 32'h39012FE9;
+		wrt_enable = 1;
+		
+		#100;
+		wrt_enable = 0;
+		
+		#100;
+		$display("Register data: %h", Q);
+		
+		#100;
+		clr = 0;
+		
+		#100;
+		$display("Cleared register data: %h", Q);
+		
+		#100;
+		$finish;
 	end
-endmodule
+	
+	always #100 clk = ~clk;
+	
+endmodule	
