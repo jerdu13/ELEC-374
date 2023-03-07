f49d78055ecb6987b23d816675b4807792bc1ec8
jacobhickling <jacobhickling14@gmail.com>
2023-03-07 12:56:51 -0500

    Update alu.v

--- CPU/alu.v
+++ CPU/alu.v
@@ -3,26 +3,28 @@ begin
 	reg [31:0] result_hi, result_lo;
 	assign z = {result_hi, result_lo};
 	
+	adder adder_circuit(a, b, result_lo, );
+	
 	always @ (*) begin
 		case (op_code)
 			5'b00011:
-				result_lo = a + b; // add
+				assign result_lo = ; // add
 			5'b00100:
-				result_lo = a - b; // sub
+				assign result_lo = a - b; // sub
 			5'b00101:
-				result_lo = a & b; // and
+				assign result_lo = a & b; // and
 			5'b00110:
-				result_lo = a | b; // or
+				assign result_lo = a | b; // or
 			5'b00111:
-				result_lo = a >> b; //shift
+				assign result_lo = a >> b; //shift
 			5'b01000:
-				result_lo = a >>>> b; // arithmetic shift
+				assign result_lo = a >>>> b; // arithmetic shift
 			5'b01001:
-				result_lo = a << b; // shift
+				assign result_lo = a << b; // shift
 			5'b01010:
-				result_lo = 1'b0; // rotate right
+				assign result_lo = 1'b0; // rotate right
 			5'b01011:
-				result_lo = 1'b0; // rotate left
+				assign result_lo = 1'b0; // rotate left
 		endcase
 	end
 endmodule
