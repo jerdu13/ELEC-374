a64d543167851b0eee03b7e4c0fb26252bc0e06d
jacobhickling <jacobhickling14@gmail.com>
2023-03-09 17:32:00 -0500

    PC finally increments properly!!!

--- CPU/pc.v
+++ CPU/pc.v
@@ -4,10 +4,12 @@ module pc(input wire clr, clk, wrt_enable, pcInc, input wire [31:0] D, output re
 	always @ (posedge clk) begin
 		if (clr)
 			Q <= 32'b0;
-		else if (pcInc == 1)
-			Q <= Q + 1;
-		else if (wrt_enable == 1)
-			Q <= D;
+		else if (wrt_enable == 1) begin
+			if (pcInc == 1)
+					Q <= Q + 1;
+			else if (wrt_enable == 1)
+				Q <= D;
+		end
 	end
 	
 endmodule
