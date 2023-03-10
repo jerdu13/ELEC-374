8098a307faf72a5953e9e36e8e62e524a319be3a
jacobhickling <jacobhickling14@gmail.com>
2023-03-09 21:58:59 -0500

    Added R0out reg, avoids high impedance implicit net generation

--- CPU/shl_tb.v
+++ CPU/shl_tb.v
@@ -2,7 +2,7 @@
 `timescale 1ns/10ps
 module shl_tb;
 	reg R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in;
-	reg R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out;
+	reg R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out;
 	reg PCin, HIin, LOin, ZHIin, ZLOin, MDRin, Inportin, Cin, IRin, Yin, MARin;
 	reg PCout, HIout, LOout, ZHIout, ZLOout, MDRout, Inportout, Cout;
 	reg Clock, MDRread, IncPC;
